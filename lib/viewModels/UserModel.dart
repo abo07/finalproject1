5499a63bc90fa6b45342525ae09d8c4698b305ad
ahmad <ahmad.abo.moch2007@gmail.com>
2024-10-26 11:32:10 +0300

    UserVM-new file

--- lib/viewModels/UserModel.dart
+++ lib/viewModels/UserModel.dart
@@ -1,33 +1,33 @@
-class User
-{
-  User(
-  {
-    this.id="",
-    this.name="",
-    this.phone="",
-    this.note="",
-
-}
-      );
+class User {
+  User({
+    this.id = "",
+    this.name = "",
+    this.phone = "",
+    this.note = "",
+    this.address="",
+  });
 
   String id;
   String name;
   String phone;
   String note;
+  String address;
+
 
   factory User.fromJson(Map<String, dynamic> json) => User(
-    id: json["id"],
-    name : json["name"],
-    phone: json["phone"],
-    note: json["note"],
+        id: json["id"],
+        name: json["name"],
+        phone: json["phone"],
+        note: json["note"],
+        address: json["address"],
 
   );
 
-  Map<String,dynamic>toJson()=> {
-    "id":id,
-    "name":name,
-    "phone":phone,
-    "note":note,
-
-  };
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "phone": phone,
+        "note": note,
+        "address":address,
+      };
 }
